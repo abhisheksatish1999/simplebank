3a62e9192d8ccc440557af16013b5554e48b26d6
Abhishek Satish <abhisheksatish10@gmail.com>
2023-10-11 22:23:51 +0530

    add multiple UT scenarios for getAccount api and made it table driven

--- api/accountHandler_test.go
+++ api/accountHandler_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -18,30 +19,78 @@ import (
 func TestGetAccountAPI(t *testing.T) {
 	account := createRandomAccount()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	testCases := []struct {
+		name          string
+		accountID     int64
+		mockclosure   func(store mock_db.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:      "1. API responds with status 200 - Success",
+			accountID: account.ID,
+			mockclosure: func(store mock_db.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), account.ID).Times(1).Return(account, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				body, err := io.ReadAll(recorder.Body)
+				require.NoError(t, err)
 
-	store := mock_db.NewMockStore(ctrl)
-	store.EXPECT().GetAccount(gomock.Any(), account.ID).Times(1).Return(account, nil)
+				var gotAccount db.Account
+				err = json.Unmarshal(body, &gotAccount)
+				require.NoError(t, err)
+				require.Equal(t, account, gotAccount)
+			},
+		},
+		{
+			name:      "2. API responds with status 404 - Not Found",
+			accountID: account.ID,
+			mockclosure: func(store mock_db.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), account.ID).Times(1).Return(db.Account{}, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name:      "3. API responds with status 500 - DB Error",
+			accountID: account.ID,
+			mockclosure: func(store mock_db.MockStore) {
+				store.EXPECT().GetAccount(gomock.Any(), account.ID).Times(1).Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name:        "4. API responds with status 400 - Invalid ID",
+			accountID:   0,
+			mockclosure: func(_ mock_db.MockStore) {},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-	server := NewServer(store)
-	recorder := httptest.NewRecorder()
+			store := mock_db.NewMockStore(ctrl)
+			tc.mockclosure(*store)
 
-	url := fmt.Sprintf("/accounts/%d", account.ID)
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	require.NoError(t, err)
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+			url := fmt.Sprintf("/accounts/%d", tc.accountID)
+			req, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
 
-	server.router.ServeHTTP(recorder, req)
-	
-	//check response code and body
-	require.Equal(t, http.StatusOK, recorder.Code)
-	body, err := io.ReadAll(recorder.Body)
-	require.NoError(t, err)
+			server.router.ServeHTTP(recorder, req)
+			tc.checkResponse(t, recorder)
+		})
 
-	var gotAccount db.Account
-	err = json.Unmarshal(body, &gotAccount)
-	require.NoError(t, err)
-	require.Equal(t,account,gotAccount)
+	}
 
 }
 
