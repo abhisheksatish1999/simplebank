69c8c23025c4c119950a15b5ed6b1f4a96427d0b
Abhishek Satish <abhisheksatish10@gmail.com>
2024-02-14 15:25:07 +0530

    * add users table and foreign key constraints * handle db errors

--- api/accountHandler.go
+++ api/accountHandler.go
@@ -6,11 +6,12 @@ import (
 
 	db "github.com/abhisheksatish1999/simplebank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR CAD"`
 }
 
 func (s *Server) createAccount(ctx *gin.Context) {
@@ -28,6 +29,13 @@ func (s *Server) createAccount(ctx *gin.Context) {
 
 	account, err := s.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
